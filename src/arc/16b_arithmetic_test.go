0d2d2c44a8aaf66ecbac11b5f0c037188f7b42b2
jisaa_yoga <giuseppe.richetti@gmail.com>
2025-04-23 11:26:20 +0200

    Add INC_DE, ADDHL_DE, DEC_DE instructions and tests

--- src/arc/16b_arithmetic_test.go
+++ src/arc/16b_arithmetic_test.go
@@ -124,3 +124,86 @@ func TestDEC_BC(t *testing.T) {
         t.Error("HL should be 0x2525. Instead got: ", cpu.BC())
     }
 }
+
+func TestINC_DE(t *testing.T) {
+
+    // Given
+    cpu := InitSM83() 
+
+    // When
+    cpu.Registers.D = 0x25
+    cpu.Registers.E = 0x26
+    cpu.Memory.RAM[0x0100] = instructions.INC_DE
+
+    expectedCycles := 2
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    // Then
+    if cyclesUsed != expectedCycles {
+        t.Error("Cycles used: ", cyclesUsed, " cycles expected: ", expectedCycles)
+    }
+
+    if cpu.DE() != 0x2527 {
+        t.Error("HL should be 0x2527. Instead got: ", cpu.DE())
+    }
+}
+
+func TestDEC_DE(t *testing.T) {
+
+    // Given
+    cpu := InitSM83() 
+
+    // When
+    cpu.Registers.D = 0x25
+    cpu.Registers.E = 0x26
+    cpu.Memory.RAM[0x0100] = instructions.DEC_DE
+
+    expectedCycles := 2
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    // Then
+    if cyclesUsed != expectedCycles {
+        t.Error("Cycles used: ", cyclesUsed, " cycles expected: ", expectedCycles)
+    }
+
+    if cpu.DE() != 0x2525 {
+        t.Error("HL should be 0x2525. Instead got: ", cpu.DE())
+    }
+}
+
+func TestADDHL_DESetsHandCFlags(t *testing.T) {
+
+    // Given
+    cpu := InitSM83() 
+
+    // When
+    cpu.Registers.H = 0x23
+    cpu.Registers.L = 0x20
+    cpu.Registers.D = 0xEF
+    cpu.Registers.E = 0x2F
+    cpu.Memory.RAM[0x0100] = instructions.ADDHL_DE
+
+    expectedCycles := 2
+    cyclesUsed := cpu.Execute(expectedCycles)
+
+    // Then
+    if cyclesUsed != expectedCycles {
+        t.Error("Cycles used: ", cyclesUsed, " cycles expected: ", expectedCycles)
+    }
+
+    if cpu.Registers.F & (1 << 6) != 0 {
+        t.Error("N flag should be 0.")
+    }
+
+    if cpu.Registers.F & (1 << 5) == 0 {
+        t.Error("H flag should be 1.")
+    }
+
+    if cpu.Registers.F & (1 << 4) == 0 {
+        t.Error("C flag should be 1.")
+    }
+
+    if cpu.HL() != 0x124F {
+        t.Error("HL should be 0x124F. Instead got: ", cpu.HL())
+    }
+}
